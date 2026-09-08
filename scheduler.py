1aac95bd7b9ddfd418a0a399483663ed3dfaa0f1
Ben Clifford <benc@hawaga.org.uk>
2026-09-08 15:34:27 +0000

    Add Permanand

--- scheduler.py
+++ scheduler.py
@@ -84,6 +84,7 @@ talk_titles_prefs = \
     ("Hai Nguyen", 5, 0, [], "A reference architecture for AI orchestration", True),
     ("Akshat Singhania", 4, 0, [], "Cognitive Routing of LLM Requests via Contextual Knowledge Building", True),
     ("Gulesh Shukla", 4, 0, ["observability"], "Observability and Academy Agents", False),
+    ("Parmanand Sinha", 6, 0, [], "No SSH Required: Building a Web-Based HPC Dispatch Service on Globus Compute", True),
   ]
 
 
